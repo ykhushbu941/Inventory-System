bec791d5d3b8fd9ceb10acf9bb07e4fae5d4f49d
Khushbu Yadav <your-email@example.com>
2026-06-22 15:41:36 +0530

    new update deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://inventory-system-lime-one.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
